78a4f7030b2316be7be71486e0a11408f7ce2d68
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-04-18 16:02:14 -0400

    Updated geway refs to methods

--- lib/apigw_construct.py
+++ lib/apigw_construct.py
@@ -22,19 +22,137 @@ class GatewayConstruct(core.Construct):
                  **kwargs) -> None:
         super().__init__(scope, construct_id)
 
+        _create_model_props = {
+            "short_url": {
+                "type": JsonSchemaType.STRING
+            }
+        }
+        _retrieve_model_props = {
+            "long_url": {
+                "type": JsonSchemaType.STRING
+            },
+            "status_code": {
+                "type": JsonSchemaType.INTEGER
+            }
+        }
+
         gw = dict(self.node.try_get_context(gw_context))
+        api_log_group = self._create_api_log_group(gw)
+        gateway = self._create_rest_api(api_log_group, gw, stage)
+        _passthrough_behavior = PassthroughBehavior.WHEN_NO_TEMPLATES
 
-        # # api gateway log groups
+        _create_response_model = self._create_response_model(
+            gateway, "CreateResponseModel", _create_model_props)
+        _retrieve_response_model = self._create_response_model(
+            gateway, "RetrieveResponseModel", _retrieve_model_props)
+        _create_response_template = self._create_response_template()
 
-        api_log_group = _logs.LogGroup(
-            self,
-            gw["gw_log_group_name"],
-            log_group_name="/aws/apigateway/" + gw["gw_log_group_name"],
-            retention=LOG_RETENTION_PERIOD,
-            removal_policy=core.RemovalPolicy.DESTROY)
+        _retrieve_response_template = self._create_retrieve_response_template()
+        _create_request_template = self._create_request_template()
+        _retrieve_request_template = self._retrieve_request_template()
 
-        # # api gateway to handle post requests
-        gateway = _api_gw.RestApi(
+        _create_lambda_integ = self._create_lambda_integration(lambda_fn_alias, _passthrough_behavior,
+                                                               _create_response_template,
+                                                               _create_request_template)
+        _retrieve_lambda_integ = self._create_lambda_integration(lambda_fn_alias2, _passthrough_behavior,
+                                                                 _retrieve_response_template,
+                                                                 _retrieve_request_template)
+
+        _create_resource = gateway.root.add_resource("create")
+
+        _create_resource.add_method(
+            "POST",
+            _create_lambda_integ,
+            api_key_required=False,
+            method_responses=[
+                MethodResponse(
+                    status_code='200',
+                    response_models={
+                        'application/json': _create_response_model}
+                ),
+                MethodResponse(
+                    status_code='400'
+                )
+            ]
+        )
+
+        _retrieve_resource = gateway.root.add_resource("{short_id}")
+
+        _retrieve_resource.add_method(
+            "GET",
+            _retrieve_lambda_integ,
+            api_key_required=False,
+            method_responses=[
+                MethodResponse(
+                    status_code='301',
+                    response_models={
+                        'application/json': _retrieve_response_model}
+                ),
+                MethodResponse(
+                    status_code='400'
+                )
+            ]
+        )
+
+        _create_resource.add_cors_preflight(
+            allow_origins=[gw["gw_origins_cors"]],
+            allow_methods=[gw["gw_origins_cors_method"]]
+        )
+
+        _retrieve_resource.add_cors_preflight(
+            allow_origins=[gw["gw_origins_cors"]],
+            allow_methods=[gw["gw_origins_cors_method"]]
+        )
+
+        core.CfnOutput(self, "ApiGwUrl", value=gateway.url)
+
+        core.CfnOutput(self,
+                       "ApiGWLogGroup",
+                       value=api_log_group.log_group_name)
+
+        self.apigw = gateway
+
+    def _create_request_template(self):
+        return textwrap.dedent(
+            """
+        #set($inputRoot = $input.path('$'))
+        {
+            "body": {
+                "long_url" : "$inputRoot.long_url"    
+            }
+        }
+        """
+        )
+
+    def _retrieve_request_template(self):
+        return textwrap.dedent(
+            """
+            {
+                "short_id": "$input.params('short_id')"
+            }
+            """
+        )
+
+    def _create_lambda_integration(self, lambda_fn_alias, _passthrough_behavior, _response_template, _request_template):
+        return _api_gw.LambdaIntegration(
+            lambda_fn_alias,
+            proxy=False,
+            passthrough_behavior=_passthrough_behavior,
+            integration_responses=[
+                _api_gw.IntegrationResponse(
+                    status_code="200",
+                    response_templates={
+                        "application/json": _response_template
+                    }
+                )
+            ],
+            request_templates={
+                "application/json": _request_template
+            }
+        )
+
+    def _create_rest_api(self, api_log_group, gw, stage):
+        return _api_gw.RestApi(
             self,
             gw["gw_name"],
             rest_api_name=gw["gw_name"],
@@ -58,8 +176,7 @@ class GatewayConstruct(core.Construct):
             },
             endpoint_configuration={
                 "types": [
-                    EndpointType.REGIONAL if gw["gw_endpoint_type"]
-                                             == "regional" else EndpointType.EDGE
+                    EndpointType.REGIONAL
                 ]
             },
             deploy=True,
@@ -67,164 +184,49 @@ class GatewayConstruct(core.Construct):
             description=gw["gw_description"],
         )
 
-        _create_response_model = gateway.add_model(
-            "CreateResponseModel",
+    def _create_api_log_group(self, gw):
+        return _logs.LogGroup(
+            self,
+            gw["gw_log_group_name"],
+            log_group_name="/aws/apigateway/" + gw["gw_log_group_name"],
+            retention=LOG_RETENTION_PERIOD,
+            removal_policy=core.RemovalPolicy.DESTROY)
+
+    def _create_response_model(self, gateway, model_name, props):
+        return gateway.add_model(
+            model_name,
             content_type="application/json",
-            model_name="CreateResponseModel",
+            model_name=model_name,
             schema={
                 "schema": JsonSchemaVersion.DRAFT4,
-                "title": "CreateResponseModel",
+                "title": model_name,
                 "type": JsonSchemaType.OBJECT,
-                "properties": {
-                    "short_url": {
-                        "type": JsonSchemaType.STRING
-                    }
-                }
+                "properties": props
 
             }
         )
 
-        _retrieve_response_model = gateway.add_model(
-            "RetrieveResponseModel",
-            model_name="RetrieveResponseModel",
-            schema={
-                "schema": JsonSchemaVersion.DRAFT4,
-                "title": "RetrieveResponseModel",
-                "type": JsonSchemaType.OBJECT,
-                "properties": {
-                    "long_url": {
-                        "type": JsonSchemaType.STRING
-                    },
-                    "status_code": {
-                        "type": JsonSchemaType.INTEGER
-                    }
-                }
-
-            }
-        )
-
-        passthrough_behavior = PassthroughBehavior.WHEN_NO_TEMPLATES
-
-        _create_response_template = textwrap.dedent(
+    @staticmethod
+    def _create_response_template():
+        return textwrap.dedent(
             """
-                    #set($inputRoot = $input.path('$'))
-                    {
-                             "short_id" : $inputRoot.body.short_id    
-                    }
-                    """
+                #set($inputRoot = $input.path('$'))
+                {
+                         "short_id" : $inputRoot.body.short_id    
+                }
+                """
         )
 
-        _retrieve_response_template = textwrap.dedent(
+    @staticmethod
+    def _create_retrieve_response_template():
+        return textwrap.dedent(
             """
-                    #set($inputRoot = $input.path('$'))
-                    {
-                             "long_url" : $inputRoot.long_url     
-                    }
-                    """
+                #set($inputRoot = $input.path('$'))
+                {
+                         "long_url" : $inputRoot.long_url     
+                }
+                """
         )
-
-        _create_lambda_integ = _api_gw.LambdaIntegration(
-            lambda_fn_alias,
-            proxy=False,
-            passthrough_behavior=passthrough_behavior,
-            integration_responses=[
-                _api_gw.IntegrationResponse(
-                    status_code="200",
-                    response_templates={
-                        "application/json": _create_response_template
-                    }
-                )
-            ],
-            request_templates={
-                "application/json": textwrap.dedent(
-                    """
-                    #set($inputRoot = $input.path('$'))
-                    {
-                        "body": {
-                            "long_url" : "$inputRoot.long_url"    
-                        }
-                    }
-                    """
-                )
-            }
-        )
-
-        _retrieve_lambda_integ = _api_gw.LambdaIntegration(
-            lambda_fn_alias2,
-            proxy=False,
-            passthrough_behavior=passthrough_behavior,
-            integration_responses=[
-                _api_gw.IntegrationResponse(
-                    status_code="200",
-                    response_templates={
-                        "application/json": _retrieve_response_template
-                    }
-                )
-            ],
-            request_templates={
-                "application/json": textwrap.dedent(
-                    """
-                    {
-                        "short_id": "$input.params('short_id')"
-                    }
-                    """
-                )
-            }
-        )
-
-        _create_resource = gateway.root.add_resource("create")
-
-        _create_resource.add_method(
-            "POST",
-            _create_lambda_integ,
-            api_key_required=False,
-            method_responses=[
-                MethodResponse(
-                    status_code='200',
-                    response_models={'application/json': _create_response_model}
-                ),
-                MethodResponse(
-                    status_code='400'
-                )
-            ]
-        )
-
-        _retrieve_resource = gateway.root.add_resource("{short_id}")
-
-        _retrieve_resource.add_method(
-            "GET",
-            _retrieve_lambda_integ,
-            api_key_required=False,
-            method_responses=[
-                MethodResponse(
-                    status_code='301',
-                    response_models={'application/json': _retrieve_response_model}
-                ),
-                MethodResponse(
-                    status_code='400'
-                )
-            ]
-        )
-
-        _create_resource.add_cors_preflight(
-            allow_origins=[gw["gw_origins_cors"]],
-            allow_methods=[gw["gw_origins_cors_method"]]
-        )
-
-        _retrieve_resource.add_cors_preflight(
-            allow_origins=[gw["gw_origins_cors"]],
-            allow_methods=[gw["gw_origins_cors_method"]]
-        )
-
-        # # Outputs
-
-        core.CfnOutput(self, "ApiGwUrl", value=gateway.url)
-
-        core.CfnOutput(self,
-                       "ApiGWLogGroup",
-                       value=api_log_group.log_group_name)
-
-        self.apigw = gateway
 
     @property
     def main_api(self) -> _api_gw.IRestApi:

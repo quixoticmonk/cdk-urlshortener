80f4d3fbc43832709bae7c50d8495a82a3895920
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-05-03 20:57:23 -0400

    Added watchful

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ from lib.lambda_construct import LambdaConstruct
 from lib.dynamo_construct import DbConstruct
 from application_construct import AppConstruct
 
+from cdk_watchful import Watchful
+
 
 class AppStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        wf = Watchful(self, 'watchful')
+        wf.watch_scope(self)
 
         _fn1 = LambdaConstruct(self, "createshortUrl", "lambda1")
         _fn2 = LambdaConstruct(self, "getUrl", "lambda2")
